994a0160554076457b7f0faf480e7eda8aa62f57
Harika-Indukuri <harika.indukuri@ihscareers.com>
2021-01-08 17:29:43 +0530

    login with jwt authentication

--- day2/src/models/user.js
+++ day2/src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -10,6 +11,7 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
+        unique: true,
         required: true,
         trim: true,
         lowercase: true,
@@ -88,8 +90,41 @@ const userSchema = new mongoose.Schema({
             required: true,
             trim: true
         }
+    }],
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
     }]
 })
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisismycode')
+
+    user.tokens = user.tokens.concat({ token: token })
+    await user.save()
+    
+    return token
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new Error('Unable to login')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if(!isMatch) {
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
+
 
 userSchema.pre('save', async function(next) {
     const user = this
